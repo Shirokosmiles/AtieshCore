31f70ffe9512e4e13c69c396912a05dedce448c8
kvipka <qvipka@gmail.com>
2016-04-19 01:21:04 +0300

    visual corrections for RE core

--- src/server/worldserver/CommandLine/CliRunnable.cpp
+++ src/server/worldserver/CommandLine/CliRunnable.cpp
@@ -105,7 +105,7 @@ void utf8print(void* /*arg*/, const char* str)
 
 void commandFinished(void*, bool /*success*/)
 {
-    printf("TC> ");
+    printf("RE> ");
     fflush(stdout);
 }
 
@@ -139,7 +139,7 @@ void CliThread()
 
     // print this here the first time
     // later it will be printed after command queue updates
-    printf("TC>");
+    printf("RE>");
 
     ///- As long as the World is running (no World::m_stopEvent), get the command line and handle it
     while (!World::IsStopped())
@@ -152,7 +152,7 @@ void CliThread()
         char commandbuf[256];
         command_str = fgets(commandbuf, sizeof(commandbuf), stdin);
 #else
-        command_str = readline("TC>");
+        command_str = readline("RE>");
         rl_bind_key('\t', rl_complete);
 #endif
 
@@ -168,7 +168,7 @@ void CliThread()
             if (!*command_str)
             {
 #if PLATFORM == PLATFORM_WINDOWS
-                printf("TC>");
+                printf("RE>");
 #else
                 free(command_str);
 #endif
@@ -179,7 +179,7 @@ void CliThread()
             if (!consoleToUtf8(command_str, command))         // convert from console encoding to utf8
             {
 #if PLATFORM == PLATFORM_WINDOWS
-                printf("TC>");
+                printf("RE>");
 #else
                 free(command_str);
 #endif
