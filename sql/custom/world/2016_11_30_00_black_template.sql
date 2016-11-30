e8cf44e1ef2fb0725a408d1d67229d40bc1c8ef2
kvipka <qvipka@gmail.com>
2016-11-30 23:08:31 +0300

    sql correction with reverting at TC version of BT for start new rewriting

---
+++ sql/custom/world/2016_11_30_00_black_template.sql
@@ -0,0 +1,26 @@
+-- SPELL Find Target (41081)
+DELETE FROM spell_linked_spell WHERE spell_trigger IN(41081, -41081);
+DELETE FROM spell_script_names WHERE spell_id=41081;
+INSERT INTO spell_script_names VALUES(41081, 'spell_gen_select_target_count_15_1');
+
+-- SPELL Curse of Agony (39672)
+DELETE FROM spell_script_names WHERE spell_id=39672;
+INSERT INTO spell_script_names VALUES(39672, 'spell_gen_select_target_count_15_1');
+
+-- SPELL L1 Arcane Charge (41357)
+DELETE FROM spell_script_names WHERE spell_id=41357;
+INSERT INTO spell_script_names VALUES(41357, 'spell_gen_select_target_count_15_1');
+
+-- SPELL L5 Arcane Charge (41360)
+DELETE FROM spell_script_names WHERE spell_id=41360;
+INSERT INTO spell_script_names VALUES(41360, 'spell_gen_100pct_count_pct_from_max_hp');
+
+-- SPELL Disgruntled (40851)
+DELETE FROM conditions WHERE SourceTypeOrReferenceId=13 AND SourceEntry=40851;
+INSERT INTO conditions VALUES(13, 7, 40851, 0, 0, 31, 0, 3, 22963, 0, 0, 0, 0, '', 'Target Bonechewer Worker');
+DELETE FROM spell_script_names WHERE spell_id=40851;
+INSERT INTO spell_script_names VALUES(40851, 'spell_gen_select_target_count_7_1');
+
+-- SPELL Rapid Shot (41172)
+DELETE FROM spell_script_names WHERE spell_id=41172;
+INSERT INTO spell_script_names VALUES(41172, 'spell_gen_select_target_count_24_1');
