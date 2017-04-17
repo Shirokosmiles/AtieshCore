151debe736e3377d58c19335a78516e5ade59fda
kvipka <qvipka@gmail.com>
2017-04-17 23:21:53 +0300

    typo

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -360,11 +360,11 @@ void WorldSession::HandleMovementOpcodes(WorldPacket& recvData)
     if (plrMover)
         sAnticheatMgr->StartHackDetection(plrMover, movementInfo, opcode);
 
-    /* process position-change */
-    WorldPacket data(opcode, recvData.size());
     if (!plrMover)
         return;
 
+    /* process position-change */
+    WorldPacket data(opcode, recvData.size());
     int64 movementTime = (int64) movementInfo.time + plrMover->m_timeSyncClockDelta; // time of the event on the server clock.
     if (movementTime < 0 || movementTime > 0xFFFFFFFF)
     {
