7ab87448660652aaa4c92a3aaf2bf4a8820e17aa
kvipka <qvipka@gmail.com>
2017-04-21 00:05:46 +0300

    fixed positive spells with SPELL_ATTR2_UNK3:             case 54646: // Focus Magic             case 19752: //Divine Intervention             case 60229: //Greatness             case 60233: //Greatness             case 60234: //Greatness

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -3361,7 +3361,24 @@ bool SpellInfo::_IsPositiveEffect(uint8 effIndex, bool deep) const
 
     // these spells must not be downscaled, thus marking them negative (see GetAuraRankForLevel)
     if (HasAttribute(SPELL_ATTR2_UNK3))
-        return false;
+    {
+        bool positive = false;
+        switch (Id)
+        {
+            case 54646: // Focus Magic
+            case 19752: //Divine Intervention
+            case 60229: //Greatness
+            case 60233: //Greatness
+            case 60234: //Greatness
+                positive = true;
+                break;
+            default:
+                break;
+        }
+
+        if (!positive)
+            return false;
+    }        
 
     switch (SpellFamilyName)
     {
