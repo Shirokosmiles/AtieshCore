bbee0a149e4630f1a5c759f1070ec376ed736bba
kvipka <qvipka@gmail.com>
2017-04-23 13:46:06 +0300

    missed delete for prevoius commit

--- sql/custom/world/2017_04_23_00_stuck_grace_effect.sql
+++ sql/custom/world/2017_04_23_00_stuck_grace_effect.sql
@@ -1,2 +1,5 @@
+DELETE FROM `spell_group_stack_rules` where `group_id` IN (1199);
 INSERT INTO `spell_group_stack_rules` (`group_id`, `stack_rule`) VALUES ('1199', '1');
+
+DELETE FROM `spell_group` where `id` IN (1199);
 INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('1199', '47930');
