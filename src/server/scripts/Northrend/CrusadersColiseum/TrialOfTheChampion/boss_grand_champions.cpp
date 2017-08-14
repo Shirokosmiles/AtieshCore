1c4e6ef7019295a7c997c70c93e6d5bdce514d24
kvipka <qvipka@gmail.com>
2017-08-14 21:46:56 +0300

    fix build

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_grand_champions.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_grand_champions.cpp
@@ -699,9 +699,9 @@ class generic_vehicleAI_toc5 : public CreatureScript
 public:
     generic_vehicleAI_toc5() : CreatureScript("generic_vehicleAI_toc5") { }
 
-    struct generic_vehicleAI_toc5AI : public npc_escortAI
+    struct generic_vehicleAI_toc5AI : public EscortAI
     {
-        generic_vehicleAI_toc5AI(Creature* creature) : npc_escortAI(creature)
+        generic_vehicleAI_toc5AI(Creature* creature) : EscortAI(creature)
         {
             Initialize();
             SetDespawnAtEnd(false);
@@ -832,10 +832,10 @@ public:
                 me->GetEntry() == VEHICLE_ARGENT_WARHORSE_COSMETIC)
                 return;
 
-            npc_escortAI::EnterEvadeMode(why);
+			EscortAI::EnterEvadeMode(why);
         }
 
-        void WaypointReached(uint32 waypointId) override
+        void WaypointReached(uint32 waypointId, uint32 /*pathId*/) override
         {
             uint32 TeamInInstance = instance->GetData(DATA_TEAM_IN_INSTANCE);
             // Grand Champions reached their final positions in the jousting event
@@ -861,7 +861,7 @@ public:
 
         void MovementInform(uint32 type, uint32 pointId) override
         {            
-            npc_escortAI::MovementInform(type, pointId);
+			EscortAI::MovementInform(type, pointId);
 
             if (type != POINT_MOTION_TYPE)
                 return;
@@ -958,7 +958,7 @@ public:
 
         void UpdateAI(uint32 uiDiff) override
         {
-            npc_escortAI::UpdateAI(uiDiff);
+			EscortAI::UpdateAI(uiDiff);
             events.Update(uiDiff);
 
             while (uint32 eventId = events.ExecuteEvent())
