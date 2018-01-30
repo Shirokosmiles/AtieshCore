30f4503fc22cfe5a7d0bc2c9c22974b3ae307443
kvipka <qvipka@gmail.com>
2018-01-30 21:17:03 +0300

    Core/Entities: Warlock pet base damage table fix
    remake of #19360 on a new branch
    tldr: suggested fix to the warlock pet base damage, placing base damage into the pet_levelstats table.
    Fills world.pet_levelstats with base damage values and stats for the 5 common warlock pets for all levels.
    
    references:
    [1] simcraft
    https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/simulationcraft/simc-335-1-win32.zip
    [2] old wowwiki
    http://wowwiki.wikia.com/wiki/Felguard_(warlock_minion)?oldid=2409171
    http://wowwiki.wikia.com/wiki/Felhunter?oldid=2714647
    http://wowwiki.wikia.com/wiki/Succubus_(warlock_minion)?oldid=2408937
    http://wowwiki.wikia.com/wiki/Voidwalker_(warlock_minion)?oldid=2353207
    http://wowwiki.wikia.com/wiki/Imp_(warlock_minion)?oldid=2410392
    [3] voidwalker tanking
    https://www.youtube.com/watch?v=SWqecvALz6Q
    https://www.youtube.com/watch?v=WO-E2craHms
    [4] my work files
    https://www.mediafire.com/folder/rbt5u2yu1w232a9,7mt04maidc7u0uu,gv97znicm4y5sb5,rv7jsd0d32gc7sc,990y8fgqhk5vgcc,je7y4dfqg574cug,quft16relsflk6f,ur88x7lih6d56x1/shared

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -663,7 +663,7 @@ typedef std::pair<ExclusiveQuestGroups::const_iterator, ExclusiveQuestGroups::co
 
 struct PetLevelInfo
 {
-    PetLevelInfo() : health(0), mana(0), armor(0)
+    PetLevelInfo() : health(0), mana(0), armor(0), minDamage(0), maxDamage(0)
     {
         for (uint16& stat : stats)
             stat = 0;
@@ -673,6 +673,8 @@ struct PetLevelInfo
     uint16 health;
     uint16 mana;
     uint16 armor;
+    uint16 minDamage;
+    uint16 maxDamage;
 };
 
 struct MailLevelReward
