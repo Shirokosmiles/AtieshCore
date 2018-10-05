96c869a9758ad3ec96d07a54144eae17a77fae43
kvipka <qvipka@gmail.com>
2018-10-05 22:31:21 +0300

    ops again

--- sql/custom/world/Content_BC/RaidDungeons/2016_11_30_00_black_template.sql
+++ sql/custom/world/Content_BC/RaidDungeons/2016_11_30_00_black_template.sql
@@ -1,7 +1,3 @@
--- SPELL L1 Arcane Charge (41357)
-DELETE FROM spell_script_names WHERE spell_id=41357;
-INSERT INTO spell_script_names VALUES(41357, 'spell_gen_select_target_count_15_1');
-
 -- SPELL L5 Arcane Charge (41360)
 DELETE FROM spell_script_names WHERE spell_id=41360;
 INSERT INTO spell_script_names VALUES(41360, 'spell_gen_100pct_count_pct_from_max_hp');
