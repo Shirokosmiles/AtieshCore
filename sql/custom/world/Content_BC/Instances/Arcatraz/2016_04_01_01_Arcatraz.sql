4ede898cce54fa7f000b0163309cfe685e496c87
kvipka <qvipka@gmail.com>
2018-10-06 01:51:15 +0300

    fix sql/custom/world/Content_BC/Instances/Arcatraz/2016_04_01_01_Arcatraz.sql

--- sql/custom/world/Content_BC/Instances/Arcatraz/2016_04_01_01_Arcatraz.sql
+++ sql/custom/world/Content_BC/Instances/Arcatraz/2016_04_01_01_Arcatraz.sql
@@ -1,30 +1,32 @@
--- Table smart_scripts
-DELETE FROM smart_scripts WHERE (source_type = 0 AND entryorguid = 20859);
-INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, event_param5, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_param4, target_x, target_y, target_z, target_o, comment) VALUES
-(20859, 0, 0, 1, 1, 0, 100, 1, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Out of Combat - Disable Combat Movement (No Repeat)'),
-(20859, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Out of Combat - Stop Attacking (No Repeat)'),
-(20859, 0, 2, 3, 4, 0, 100, 0, 0, 0, 0, 0, 0, 11, 15620, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Cast \'Shoot\''),
-(20859, 0, 3, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 23, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Increment Phase'),
-(20859, 0, 4, 5, 9, 0, 100, 0, 5, 30, 2300, 5000, 0, 11, 15620, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Shoot\''),
-(20859, 0, 5, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Set Sheath Ranged'),
-(20859, 0, 6, 7, 4, 0, 100, 0, 0, 0, 0, 0, 0, 11, 22907, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Cast \'Shoot\''),
-(20859, 0, 7, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 23, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Increment Phase'),
-(20859, 0, 8, 9, 9, 0, 100, 0, 5, 30, 2300, 5000, 0, 11, 22907, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Shoot\''),
-(20859, 0, 9, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Set Sheath Ranged'),
-(20859, 0, 10, 11, 9, 0, 100, 1, 25, 80, 0, 0, 0, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 25-80 Range - Enable Combat Movement (No Repeat)'),
-(20859, 0, 11, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 25-80 Range - Start Attacking (No Repeat)'),
-(20859, 0, 12, 13, 9, 0, 100, 1, 0, 5, 0, 0, 0, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Enable Combat Movement (No Repeat)'),
-(20859, 0, 13, 14, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Set Sheath Melee (No Repeat)'),
-(20859, 0, 14, 25, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Start Attacking (No Repeat)'),
-(20859, 0, 15, 16, 9, 0, 100, 1, 5, 15, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-15 Range - Disable Combat Movement (No Repeat)'),
-(20859, 0, 16, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-15 Range - Stop Attacking (No Repeat)'),
-(20859, 0, 21, 22, 9, 0, 100, 0, 5, 30, 5000, 8000, 0, 11, 36609, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Arcane Shot\''),
-(20859, 0, 22, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Set Sheath Ranged'),
-(20859, 0, 23, 0, 7, 0, 100, 0, 0, 0, 0, 0, 0, 40, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Evade - Set Sheath Melee'),
-(20859, 0, 24, 22, 9, 0, 100, 0, 5, 30, 5000, 8000, 0, 11, 38807, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Arcane Shot\''),
-(20859, 0, 25, 26, 61, 0, 100, 1, 0, 0, 0, 0, 0, 11, 35963, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Cast \'Improved Wing Clip\' (No Repeat)'),
-(20859, 0, 26, 27, 61, 0, 100, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Disable Combat Movement (No Repeat)'),
-(20859, 0, 27, 5, 61, 0, 100, 0, 0, 0, 0, 0, 0, 89, 15, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Start Random Movement (No Repeat)');
+-- Arcatraz Warder SAI
+SET @ENTRY := 20859;
+UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
+INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, event_param5, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES
+(@ENTRY, 0, 0, 1, 1, 0, 100, 1, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Out of Combat - Disable Combat Movement (No Repeat)'),
+(@ENTRY, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Out of Combat - Stop Attacking (No Repeat)'),
+(@ENTRY, 0, 2, 3, 4, 0, 100, 0, 0, 0, 0, 0, 0, 11, 15620, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Cast \'Shoot\''),
+(@ENTRY, 0, 3, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 23, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Increment Phase'),
+(@ENTRY, 0, 4, 5, 9, 0, 100, 0, 5, 30, 2300, 5000, 0, 11, 15620, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Shoot\''),
+(@ENTRY, 0, 5, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Set Sheath Ranged'),
+(@ENTRY, 0, 6, 7, 4, 0, 100, 0, 0, 0, 0, 0, 0, 11, 22907, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Cast \'Shoot\''),
+(@ENTRY, 0, 7, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 23, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Aggro - Increment Phase'),
+(@ENTRY, 0, 8, 9, 9, 0, 100, 0, 5, 30, 2300, 5000, 0, 11, 22907, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Shoot\''),
+(@ENTRY, 0, 9, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Set Sheath Ranged'),
+(@ENTRY, 0, 10, 11, 9, 0, 100, 1, 25, 80, 0, 0, 0, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 25-80 Range - Enable Combat Movement (No Repeat)'),
+(@ENTRY, 0, 11, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 25-80 Range - Start Attacking (No Repeat)'),
+(@ENTRY, 0, 12, 13, 9, 0, 100, 1, 0, 5, 0, 0, 0, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Enable Combat Movement (No Repeat)'),
+(@ENTRY, 0, 13, 14, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Set Sheath Melee (No Repeat)'),
+(@ENTRY, 0, 14, 25, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Start Attacking (No Repeat)'),
+(@ENTRY, 0, 15, 16, 9, 0, 100, 1, 5, 15, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-15 Range - Disable Combat Movement (No Repeat)'),
+(@ENTRY, 0, 16, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-15 Range - Stop Attacking (No Repeat)'),
+(@ENTRY, 0, 21, 22, 9, 0, 100, 0, 5, 30, 5000, 8000, 0, 11, 36609, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Arcane Shot\''),
+(@ENTRY, 0, 22, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 40, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Set Sheath Ranged'),
+(@ENTRY, 0, 23, 0, 7, 0, 100, 0, 0, 0, 0, 0, 0, 40, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - On Evade - Set Sheath Melee'),
+(@ENTRY, 0, 24, 22, 9, 0, 100, 0, 5, 30, 5000, 8000, 0, 11, 38807, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 5-30 Range - Cast \'Arcane Shot\''),
+(@ENTRY, 0, 25, 26, 61, 0, 100, 1, 0, 0, 0, 0, 0, 11, 35963, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Cast \'Improved Wing Clip\' (No Repeat)'),
+(@ENTRY, 0, 26, 27, 61, 0, 100, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Disable Combat Movement (No Repeat)'),
+(@ENTRY, 0, 27, 5, 61, 0, 100, 0, 0, 0, 0, 0, 0, 89, 15, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Arcatraz Warder - Within 0-5 Range - Start Random Movement (No Repeat)');
 
 -- Arcatraz Defender SAI
 SET @ENTRY := 20857;
