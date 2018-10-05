79a1bb040c801889424f05bef29e09bcbd2e0926
kvipka <qvipka@gmail.com>
2018-10-05 19:50:06 +0300

    Scripts/Ulduar: Boss Kologarn #22386 Changes proposed:
    Script and enable all boss achievements;
    Fix issues with Arms and evade;
    Fix all visual issues with Focused Eyebeam spell;
    Added missing spells;
    Script Npc Rumble;
    Add correct boss spawn, by areatrigger;
    Implemented Kologarn Pit Kill bunny;
    Updated script register model.

--- sql/custom/world/2017_11_10_01_SQL_clear_db.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_illidan_find_target');
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (41081, 'spell_illidan_find_target');
-
-UPDATE `gameobject_template` SET `ScriptName` = '' WHERE (`entry`='194675');
--- sql/custom/world/2018_02_05_00_cheat_death.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_script_names` where `spell_id` IN (45182);
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(45182, 'spell_rog_cheat_death_dummy');
