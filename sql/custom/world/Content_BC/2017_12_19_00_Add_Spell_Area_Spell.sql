293454e75145b14f70cb292059cc1304406d40b1
kvipka <qvipka@gmail.com>
2018-11-15 12:05:04 +0300

    fix autocast for spell_area in hellfire and nagrand

--- sql/custom/world/Content_BC/2017_12_19_00_Add_Spell_Area_Spell.sql
+++ sql/custom/world/Content_BC/2017_12_19_00_Add_Spell_Area_Spell.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell_area` WHERE `spell` IN (32049, 32071, 33795);
 INSERT INTO `spell_area` VALUES 
-('32049', '3483', '0', '0', '0', '0', '2', '1', '64', '11'), -- Hellfire Peninsula Horde
-('32071', '3483', '0', '0', '0', '0', '2', '1', '64', '11'), -- Hellfire Peninsula Alliance
-('33795', '3518', '0', '0', '0', '0', '2', '1', '64', '11'); -- Nagrand 
+('32049', '3483', '0', '0', '0', '0', '2', '0', '64', '11'), -- Hellfire Peninsula Horde
+('32071', '3483', '0', '0', '0', '0', '2', '0', '64', '11'), -- Hellfire Peninsula Alliance
+('33795', '3518', '0', '0', '0', '0', '2', '0', '64', '11'); -- Nagrand 
