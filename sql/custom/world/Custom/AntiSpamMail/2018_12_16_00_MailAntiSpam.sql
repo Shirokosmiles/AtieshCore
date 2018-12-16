2a3ef0ef1d8c2fd2ab111851b5b1d4a3b7f4dc38
kvipka <qvipka@gmail.com>
2018-12-16 22:11:38 +0300

    loc8 wtf?

--- sql/custom/world/Custom/AntiSpamMail/2018_12_16_00_MailAntiSpam.sql
+++ sql/custom/world/Custom/AntiSpamMail/2018_12_16_00_MailAntiSpam.sql
@@ -1,2 +1,3 @@
 -- Antispam mail
-INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES ('11124', 'You have sent too many mails than available %d in a short time interval.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '¬ы отправили слишком много писем за короткий интервал времени, чем это допустимо %d.');
+DELETE FROM `trinity_string` where `entry` IN (11124);
+INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES ('11124', 'You have sent too many mails than available %d in a short time interval.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
