4cb86f8a99759f680dfd89d3e2db6e14336d449e
kvipka <qvipka@gmail.com>
2018-12-22 20:04:26 +0300

    [3.3.5] Map-wide update ticks #22268

--- src/server/game/Grids/Dynamic/TypeContainerVisitor.h
+++ src/server/game/Grids/Dynamic/TypeContainerVisitor.h
@@ -19,41 +19,41 @@
 #ifndef TRINITY_TYPECONTAINERVISITOR_H
 #define TRINITY_TYPECONTAINERVISITOR_H
 
-/*
- * @class TypeContainerVisitor is implemented as a visitor pattern.  It is
- * a visitor to the TypeContainerList or TypeContainerMapList.  The visitor has
- * to overload its types as a visit method is called.
- */
+ /*
+  * @class TypeContainerVisitor is implemented as a visitor pattern.  It is
+  * a visitor to the TypeContainerList or TypeContainerMapList.  The visitor has
+  * to overload its types as a visit method is called.
+  */
 
 #include "Define.h"
 #include "Dynamic/TypeContainer.h"
 
-// forward declaration
+  // forward declaration
 template<class T, class Y> class TypeContainerVisitor;
 
 // visitor helper
-template<class VISITOR, class TYPE_CONTAINER> void VisitorHelper(VISITOR &v, TYPE_CONTAINER &c)
+template<class VISITOR, class TYPE_CONTAINER> void VisitorHelper(VISITOR& v, TYPE_CONTAINER& c)
 {
     v.Visit(c);
 }
 
 // terminate condition container map list
-template<class VISITOR> void VisitorHelper(VISITOR &/*v*/, ContainerMapList<TypeNull> &/*c*/) { }
+template<class VISITOR> void VisitorHelper(VISITOR&, ContainerMapList<TypeNull>&) { }
 
-template<class VISITOR, class T> void VisitorHelper(VISITOR &v, ContainerMapList<T> &c)
+template<class VISITOR, class T> void VisitorHelper(VISITOR &v, ContainerMapList<T>& c)
 {
     v.Visit(c._element);
 }
 
 // recursion container map list
-template<class VISITOR, class H, class T> void VisitorHelper(VISITOR &v, ContainerMapList<TypeList<H, T> > &c)
+template<class VISITOR, class H, class T> void VisitorHelper(VISITOR &v, ContainerMapList<TypeList<H, T>>& c)
 {
     VisitorHelper(v, c._elements);
     VisitorHelper(v, c._TailElements);
 }
 
 // for TypeMapContainer
-template<class VISITOR, class OBJECT_TYPES> void VisitorHelper(VISITOR &v, TypeMapContainer<OBJECT_TYPES> &c)
+template<class VISITOR, class OBJECT_TYPES> void VisitorHelper(VISITOR &v, TypeMapContainer<OBJECT_TYPES>& c)
 {
     VisitorHelper(v, c.GetElements());
 }
@@ -84,20 +84,26 @@ void VisitorHelper(VISITOR& v, TypeUnorderedMapContainer<OBJECT_TYPES, KEY_TYPE>
 template<class VISITOR, class TYPE_CONTAINER>
 class TypeContainerVisitor
 {
-    public:
-        TypeContainerVisitor(VISITOR &v) : i_visitor(v) { }
+public:
+    TypeContainerVisitor(VISITOR &v) : i_visitor(v) { }
 
-        void Visit(TYPE_CONTAINER& c)
-        {
-            VisitorHelper(i_visitor, c);
-        }
+    void Visit(TYPE_CONTAINER& c)
+    {
+        VisitorHelper(i_visitor, c);
+    }
 
-        void Visit(TYPE_CONTAINER const& c) const
-        {
-            VisitorHelper(i_visitor, c);
-        }
-
-    private:
-        VISITOR &i_visitor;
+private:
+    VISITOR &i_visitor;
 };
+
+namespace TypeContainer
+{
+    // visit helper
+    template <class VISITOR, class CONTAINER>
+    void Visit(CONTAINER& c, VISITOR& v)
+    {
+        TypeContainerVisitor<VISITOR, CONTAINER>(v).Visit(c);
+    }
+}
+
 #endif
