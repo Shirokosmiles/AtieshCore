a34c20d76a2c351b5c33bda6104068daa746936a
kvipka <qvipka@gmail.com>
2019-08-08 09:20:30 +0300

    fix linux startup (sql CURRENT_TIMESTAMP)

--- sql/custom/characters/GuildSystem/2019_08_16_00_guild_wars.sql
+++ sql/custom/characters/GuildSystem/2019_08_16_00_guild_wars.sql
@@ -8,7 +8,7 @@ CREATE TABLE `guild_wars`  (
   `Attacker_guild_Id` int(10) UNSIGNED NOT NULL,
   `Defender_guild_Id` int(10) UNSIGNED NOT NULL,
   `time_of_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `time_of_end` timestamp,
+  `time_of_end` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `winner` int(10) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARACTER SET = utf8;
