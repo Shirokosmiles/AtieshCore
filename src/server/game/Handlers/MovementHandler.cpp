7bd32b7a16e17c22d35416e16c7ea51b8401381e
kvipka <qvipka@gmail.com>
2020-01-14 22:03:39 +0300

    correction from Aokromes info

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -475,7 +475,7 @@ void WorldSession::HandleMovementOpcodes(WorldPacket& recvData)
     }
 
     // fall damage generation (ignore in flight case that can be triggered also at lags in moment teleportation to another map).
-    if (opcode == MSG_MOVE_FALL_LAND && plrMover && !plrMover->IsInFlight())
+    if (opcode == MSG_MOVE_FALL_LAND && plrMover && !plrMover->IsInFlight() && plrMover->IsFlying())
     {
         plrMover->HandleFall(movementInfo);
         plrMover->SetJumpingbyOpcode(false);
@@ -504,9 +504,9 @@ void WorldSession::HandleMovementOpcodes(WorldPacket& recvData)
     if (opcode == MSG_MOVE_JUMP)
     {
         jumpopcode = true;
-        plrMover->SetFallInformation(plrMover->GetPositionZ());
         if (plrMover)
         {
+            plrMover->SetFallInformation(plrMover->GetPositionZ());
             plrMover->SetUnderACKmount();
             if (mover->IsFalling())
             {
