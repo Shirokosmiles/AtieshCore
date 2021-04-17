e101eda6b0663b72d079465be6de62e210030f74
kvipka <qvipka@gmail.com>
2021-04-17 14:34:05 +0300

    remove double handle

--- src/server/scripts/Northrend/zone_wintergrasp.cpp
+++ src/server/scripts/Northrend/zone_wintergrasp.cpp
@@ -669,17 +669,6 @@ struct wg_guardAI : public ScriptedAI
         events.ScheduleEvent(EVENT_BLOOD_HOWL, randtime(10s, 17s));
     }
 
-    void JustDied(Unit* killer) override
-    {
-        if (!killer || killer->GetTypeId() != TYPEID_PLAYER)
-            return;
-
-        if (!sWintergraspMgr->IsWarTime())
-            return;
-
-        sWintergraspMgr->HandlePromotion(killer->ToPlayer(), me);
-    }
-
     void UpdateAI(uint32 diff) override
     {
         if (!me || !me->IsAlive())
