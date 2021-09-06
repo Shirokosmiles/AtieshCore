c77a8c72559725c5d27acf74986ee52b735d8b21
kvipka <qvipka@gmail.com>
2021-09-06 20:37:54 +0300

    remove old custom sql

--- sql/custom/world/Spells/dk/2016_01_20_00_dk_rune_weapon.sql
+++
@@ -1,5 +0,0 @@
-UPDATE creature_template SET `ScriptName`="npc_pet_dk_rune_weapon" WHERE entry=27893;
-
-DELETE FROM `spell_script_names` where `spell_id` IN (49028);
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(49028, 'spell_dk_dancing_rune_weapon');
