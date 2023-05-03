eb2dbcdcc9a55eda64ba3f98890777dedec2b705
kvipka <qvipka@gmail.com>
2023-05-03 16:58:02 +0300

    move clean configs in own sql (full data)

--- sql/custom/world/ConfigDB/custom_world_config/2023_05_02_01_world_custom_config.sql
+++ sql/custom/world/ConfigDB/custom_world_config/2023_05_02_01_world_custom_config.sql
@@ -1,3 +1,17 @@
+-- ----------------------------
+-- Table structure for world_config
+-- ----------------------------
+DROP TABLE IF EXISTS `world_custom_config`;
+CREATE TABLE `world_custom_config`  (
+  `OptionName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `Type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Support types - bool, int, float',
+  `IDInTypeGroup` int(10) NOT NULL,
+  `DefaultValue` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `CustomValue` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Description` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`OptionName`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
 DELETE FROM `world_custom_config` WHERE `Type` = 'bool' AND `IDInTypeGroup` between 0 and 50;
 INSERT INTO `world_custom_config`(`OptionName`, `Type`, `IDInTypeGroup`, `DefaultValue`, `CustomValue`, `Description`) VALUES 
 -- AT configs
@@ -52,3 +66,43 @@ INSERT INTO `world_custom_config`(`OptionName`, `Type`, `IDInTypeGroup`, `Defaul
 ('VAS.Unit.Enabled', 'bool', 48, '0', NULL, 'VAS Auto Balance system'),
 ('vmap.enableLOS', 'bool', 49, '1', NULL, 'VMmap support for line of sight and height calculation.'),
 ('vmap.enableHeight', 'bool', 50, '1', NULL, 'VMmap support for line of sight and height calculation.');
+
+DELETE FROM `world_custom_config` WHERE `Type` = 'int' AND `IDInTypeGroup` between 0 and 24;
+INSERT INTO `world_custom_config`(`OptionName`, `Type`, `IDInTypeGroup`, `DefaultValue`, `CustomValue`, `Description`) VALUES 
+-- AT configs
+('VanishVisionTimer', 'int', 0, '600', NULL, 'Vanish Fade Delay (vanish evade all spells without CC control in this time) (in ms)'),
+('VanishCCbreakTimer', 'int', 1, '300', NULL, 'Vanish Should evade CC spells too, when casted, but it\'s a little lime.'),
+('AntiCheats.FlyHackTimer', 'int', 2, '3000', NULL, 'Timer for AntiCheat FlyHack check #Default: 3000 - 1 check in 1 second'),
+('FLAB.Script.MaxChars', 'int', 3, '0', NULL, 'Count of characters for give a bonus #Default:     0 - it should be a first character on acc, right?'),
+('FLAB.Script.BonusMoney', 'int', 4, '10000000', NULL, 'Count of money ( 1000 gold )'),
+('PVPBonusCapReward.MaxCap', 'int', 5, '1500', NULL, 'Max values for Weekly Cap'),
+('PVPBonusCapReward.CapChanges.Win', 'int', 6, '180', NULL, 'Value for cap after Win'),
+('PVPBonusCapReward.CapChanges.Lose', 'int', 7, '60', NULL, 'Value for cap after Lose'),
+('Antispam.Mail.Timer', 'int', 8, '3600', NULL, 'Timer for possible next sending count of mails by mailbox'),
+('Antispam.Mail.Count', 'int', 9, '10', NULL, 'How many mails you can sent in game in 1 period of timer'),
+('Antispam.Mail.Controller', 'int', 10, '100', NULL, 'How many mails player can hold in mailbox, include mail in future (from auctionhouse and calendar).'),
+('BAG.Script.BonusBagsID', 'int', 11, '51809', NULL, 'itemid of bags ( 51809 )'),
+('GSystem.GuildWars.Minimal.PlayerCount', 'int', 12, '10', NULL, 'minimal count of members in guild for participation in GW'),
+('GSystem.GuildWars.Minimal.Delta.PreviousStartTimer', 'int', 13, '3600', NULL, 'minimal timer for guild from previous start war time for starting a new war'),
+('GSystem.GuildWars.Minimal.Delta.PreviousEndTimer', 'int', 14, '3600', NULL, 'minimal timer for guild from previous end war time for starting a new war'),
+('GSystem.GuildLevel.Exp.ForArena', 'int', 15, '5', NULL, 'exp of guild for Win on Arena of each guild member'),
+('GSystem.GuildLevel.Exp.ForBG', 'int', 16, '1', NULL, 'exp of guild for Win on BG of each guild member'),
+('GSystem.GuildLevel.Exp.ForLFG', 'int', 17, '1', NULL, 'exp of guild for Win on LFG of each guild member'),
+('GSystem.GuildWars.Exp.ForEnemyGuildKill', 'int', 18, '1', NULL, 'reward guild by exp for killing members of enemy guild (in GW status). Depend from GSystem.GuildLevel.Enabled'),
+('GSystem.GuildWars.Exp.ForWinnerOfGW', 'int', 19, '250', NULL, 'reward winner guild by exp for Win in Guild Wars'),
+('GSystem.GuildWars.Exp.ForLoserOfGW', 'int', 20, '100', NULL, 'reward winner guild by exp for Lose in Guild Wars'),
+('GSystem.GuildLevel.LvL.ForMyBank', 'int', 21, '5', NULL, 'Level of guild for opening MyBank func'),
+('GSystem.GuildLevel.LvL.ForRepair', 'int', 22, '3', NULL, 'Level of guild for opening Repair func'),
+('DalaranGEvent.Min.Players', 'int', 23, '5', NULL, 'DalaranCrater Event minimal players');
+
+DELETE FROM `world_custom_config` WHERE `Type` = 'rate' AND `IDInTypeGroup` between 0 and 7;
+INSERT INTO `world_custom_config`(`OptionName`, `Type`, `IDInTypeGroup`, `DefaultValue`, `CustomValue`, `Description`) VALUES 
+-- AT configs
+('Rate.XP.Kill.Premium', 'rate', 0, '1', NULL, 'Individual rate for vip player'),
+('Rate.XP.Quest.Premium', 'rate', 1, '1', NULL, 'Individual rate for vip player'),
+('Rate.Honor.Premium', 'rate', 2, '1', NULL, 'Individual rate for vip player'),
+('Rate.Reputation.Gain.Premium', 'rate', 3, '1', NULL, 'Individual rate for vip player'),
+('GSystem.GuildLevel.Rate.XP.Bonus', 'rate', 4, '2', NULL, 'Rate for character experience when guild >= 2 level'),
+('VAS.Creature.InZone.Damage', 'rate', 5, '1', NULL, 'Change HP and Damage value for Units in Dungeons in % #Default: 100 - (full HP and Damage) 10 - (units have only 10% of HP or can deal damage 10% of base damage value)'),
+('VAS.Creature.InZone.SpellDamage', 'rate', 6, '1', NULL, 'Change HP and Damage value for Units in Dungeons in % #Default: 100 - (full HP and Damage) 10 - (units have only 10% of HP or can deal damage 10% of base damage value)'),
+('VAS.Creature.InZone.Health', 'rate', 7, '1', NULL, 'Change HP and Damage value for Units in Dungeons in % #Default: 100 - (full HP and Damage) 10 - (units have only 10% of HP or can deal damage 10% of base damage value)');
